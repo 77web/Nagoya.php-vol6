e7ac72232c876952410659048a2095ecb6af27d5
77web <info@77-web.com>
2014-08-17 14:43:04 +0900

    処理の流れを仮で考えた

--- src/EnglishClass.php
+++ src/EnglishClass.php
@@ -4,4 +4,20 @@ namespace Nagoya;
 
 class EnglishClass
 {
+    /**
+     * @param string $input
+     * @return string
+     */
+    public function run($input)
+    {
+        // 入力文字列をStaffの配列に変換する
+        // $staffs = $this->inputParser->convertToStaffs($input);
+
+        // 割り振りを行う
+        // $classes = $this->classGenerator->generate($staffs);
+
+        // 出力を整える
+        // $output = $this->outputFormatter->format($classes);
+        // return $output;
+    }
 }
--- tests/EnglishClassTest.php
+++ tests/EnglishClassTest.php
@@ -4,4 +4,27 @@ namespace Nagoya;
 
 class EnglishClassTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @test
+     * @param string $input
+     * @param string $expectedOutput
+     * @dataProvider provideInputAndOutput
+     */
+    public function test_actual_run($input, $expectedOutput)
+    {
+        $englishClass = new EnglishClass();
+        $output = $englishClass->run($input);
+
+        $this->assertEquals($expectedOutput, $output);
+    }
+
+    /**
+     * @return array
+     */
+    public function provideInputAndOutput()
+    {
+        return [
+            ['1_12345', '1_1'],
+        ];
+    }
 }
